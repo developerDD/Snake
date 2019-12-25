15e25ea335fd3faac2e9ac3b62a7c8ecd2b1a1d9
DDfire <developerdd@ukr.net>
2019-12-25 13:34:26 +0200

    new branch

--- src/GameField.java
+++ src/GameField.java
@@ -24,7 +24,7 @@ public class GameField extends JPanel implements ActionListener {
 
 
     public GameField() {
-        setBackground(Color.black);
+        setBackground(Color.CYAN);
         loadImages();
         initGame();
     }
